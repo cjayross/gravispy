6ebd16cffb2eda05ffe8cbc54075588b1879e1f1
Calvin Ross <calvinjayross@gmail.com>
2018-09-25 21:11:23 -0500

    included setup information

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+        name='gravispy',
+        version='0.1-dev',
+        packages=['gravispy',],
+        long_description=open('README.md').read()
+        )
