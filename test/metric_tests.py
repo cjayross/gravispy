198da9ff334194d1c333925de8055952a2a1acfa
Calvin Ross <calvinjayross@gmail.com>
2018-09-25 22:07:03 -0500

    added time metrics

--- test/metric_tests.py
+++ test/metric_tests.py
@@ -1,11 +1,22 @@
+from time import time
+t0 = time()
+def time_stamp (message):
+    print(message + ': %f' % (time() - t0))
+
 from gravispy.geom.metric import *
 from gravispy.ext.gravipy import *
+time_stamp('Loaded modules')
+
 t,r,theta,phi,M,m,E,L = symbols('t r theta phi M m E L', real=True)
 F = Function('F')
 G = Function('G')
 chi = Coordinates('chi', [t,r,theta,phi])
 
 g = Schwarzschild('g', chi, M)
+time_stamp('Loaded metric tensor')
+
 g.set_principal_function(F, G, [m, E, L])
 geodesics = g.geodesic_functions()
+time_stamp('Defined Geodesic equations')
 HJ = g.hamilton_jacobi()
+time_stamp('Defined Hamilton-Jacobi equation')
