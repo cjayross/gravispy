2bd254ec89ded992b217a610c5141402c6cfc9e9
Calvin Ross <calvinjayross@gmail.com>
2018-10-25 11:13:14 -0500

    restructured lensing functions

--- gravispy/geom/lensing.py
+++ gravispy/geom/lensing.py
@@ -1,3 +1,14 @@
-class Lens (object):
-    def __init__(self, *args, **kwargs):
-        pass
+import numpy as np
+from numpy.linalg import norm
+from .geom import Ray, plane_intersect
+
+def thin_lens(plane, ray_list, deflection_function):
+    ret = []
+    for ray in ray_list:
+        T = plane_intersect(plane, ray)
+        if T is np.NaN:
+            ret.append(None)
+            continue
+
+def radial_thin_lens(plane, ray_list, deflection_function):
+    pass
