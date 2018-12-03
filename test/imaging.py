57badc809a0f6f53ed2974826d99495fdb1463f8
Calvin Ross <calvinjayross@gmail.com>
2018-12-03 00:53:42 -0600

    began testing black_hole.py

--- test/imaging.py
+++ test/imaging.py
@@ -12,12 +12,12 @@ S = metric.Schwarzschild(1, [t,r,th,ph], timelike=False, lambdify_modules='numpy
 
 #np.seterr(all='raise')
 
-img = Image.open('nebula.png')
+img = Image.open('blue_marble.png')
 print('Generating lens map')
 #lens_map = model.generate_lens_map(lensing.trivial_lens, img.size, (3e+3,3e+7))
 #lens_map = model.generate_lens_map(lensing.thin_lens, img.size, (3e+3,3e+7,lensing.snells_law,1.5))
 #lens_map = model.generate_lens_map(lensing.schwarzschild_thin_lens, img.size, (3e+1,3e+7,S))
-lens_map = model.generate_lens_map(lensing.schwarzschild_lens, img.size, (10,3e+7,S))
+lens_map = model.generate_lens_map(lensing.schwarzschild_lens, img.size, (2.1,3e+7,S))
 #lens_map = model.generate_lens_map(lensing.static_spherical_lens, img.size, (10,3e+7,S))
 print('Applying lens to image')
 model.apply_lensing(img, lens_map)
